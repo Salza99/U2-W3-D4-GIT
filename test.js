a54c7f992ddb3ef0b9ce11bbcf2e32fa6561bfd3
Salza99 <137687037+Salza99@users.noreply.github.com>
2023-11-16 15:01:52 +0100

    commit su branch-da-eliminare di modifiche che verranno mergiate su develop alle quali seguirà la cancellazione del branch

--- test.js
+++ test.js
@@ -1,2 +1,3 @@
 // queste modifiche servono per poter committare sul branch develop e successivamente mergiare sul main
 // questo commento creerà una conflittualità con il branch main al merge!!
+// queste modifiche verranno mergiate su develop e in seguito il branch "branch-da-eliminare" verrà cancellato
