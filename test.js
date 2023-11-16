e6922ec6570db6165707c808d299a49f1688f0dc
Salza99 <137687037+Salza99@users.noreply.github.com>
2023-11-16 14:53:37 +0100

    commit sul main al fine di generare una conflittualità al merge con develop

--- test.js
+++ test.js
@@ -1,1 +1,2 @@
 // queste modifiche servono per poter committare sul branch develop e successivamente mergiare sul main
+// questo è un commento sul branch main che darà una conflittualità sul branch develop
