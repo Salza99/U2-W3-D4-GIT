171febde32e29e1884bc793bc5f1861f0b5ce225
Salza99 <137687037+Salza99@users.noreply.github.com>
2023-11-16 15:09:09 +0100

    questo commit verrà segnato con un tag nella storia della repository

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
 // queste modifiche servono per poter committare sul branch develop e successivamente mergiare sul main
 // questo commento creerà una conflittualità con il branch main al merge!!
 // queste modifiche verranno mergiate su develop e in seguito il branch "branch-da-eliminare" verrà cancellato
+// questo commento verrà contrassegnato da un tag nel commit al fine di rendere evidente una modifica importante
